f9a64a58a428dc1cfc068bbd4c8ab4940d2b2a80
Alejandrocg024 <alecamgal1@alum.us.es>
2023-10-20 17:25:49 +0200

    Result.html done

--- sqliBypass/app.py
+++ sqliBypass/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import sqlite3 as sql
 app = Flask(__name__)
 
-@app.route('/login',methods = ['POST', 'GET'])
+@app.route('/',methods = ['POST', 'GET'])
 def addrec():
    if request.method == 'POST':
       try:
@@ -32,10 +32,6 @@ def addrec():
            return render_template('login.html',msg = msg)
    else:
       return render_template('login.html')
-
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    return render_template('index.html')
 
 if __name__ == '__main__':
    app.run(debug = True)
