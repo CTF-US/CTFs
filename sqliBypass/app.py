3b019e66374f2e6a32e010f5d85e10c4b7ebe7d0
Alejandrocg024 <alecamgal1@alum.us.es>
2023-10-20 17:07:47 +0200

    Error in login

--- sqliBypass/app.py
+++ sqliBypass/app.py
@@ -8,27 +8,32 @@ def addrec():
       try:
          username = request.form['username']
          password = request.form['password']
-         
+         correct = False
          with sql.connect("database.db") as con:
             cur = con.cursor()
             cur.execute("select * from users where username = '" + username + "' and password = '" + password + "'")
             row = cur.fetchall()
 
             if len(row) > 0:
+               correct = True
                msg = "flag{example}"
             else:
-               msg = "Invalid User/Password"
+               msg = " Los datos introducidos no fueron correctos."
+               return render_template('login.html')
       except Exception as e:
          con.rollback()
          print(e)
          msg = "error in login"
       finally:
         con.close()
-        return render_template("result.html",msg = msg)
+        if correct:
+           return render_template("result.html",msg = msg)
+        else:
+           return render_template('login.html',msg = msg)
    else:
       return render_template('login.html')
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('index.html')
 
