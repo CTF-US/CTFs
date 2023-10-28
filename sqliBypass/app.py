9469e685b8310896fd77007056a30511c16c2de3
gonriblun <gonriblun@alum.us.es>
2023-10-28 14:10:24 +0200

    SqliByPass: Crea nuevo reto y arregla conexion db
    Crea nuevo reto Fuerza Bruta y arregla error de conexion con la bd

--- sqliBypass/app.py
+++ sqliBypass/app.py
@@ -2,32 +2,44 @@ from flask import Flask, render_template, request
 import sqlite3 as sql
 app = Flask(__name__)
 
+@app.route('/robots.txt')
+def getHint():
+   return "User-agent: * <br>Disallowed: /wordpress/defaultCreds"
+
 @app.route('/login',methods = ['POST', 'GET'])
 def addrec():
    if request.method == 'POST':
       try:
          username = request.form['username']
          password = request.form['password']
-         correct = False
-         with sql.connect("database.db") as con:
-            cur = con.cursor()
-            cur.execute("select * from users where username = '" + username + "' and password = '" + password + "'")
-            row = cur.fetchall()
-
-            if len(row) > 0:
-               correct = True
-               msg = "flag{example}"
+         correctDefaultCred = False
+         correctSqlinyection = False
+         
+         with sql.connect("sqliBypass/database.db") as con:
+            if username =='username' and password == 'password':
+               correctDefaultCred=True
+               msg ="flag{pRi_meRa__FlAg_deL_Reto_fAltaOtRa}"
             else:
-               msg = " Los datos introducidos no fueron correctos."
-               return render_template('login.html')
+               cur = con.cursor()
+               cur.execute("select * from users where username = '" + username + "' and password = '" + password + "'")
+               row = cur.fetchall()
+               if len(row) > 0:
+                  correctSqlinyection = True
+                  msg = "flag{seG_unDa__FlAg_deL_Reto_fAltaOtRa}"
+               else:
+                  msg = " Los datos introducidos no fueron correctos."
+                  return render_template('login.html')
+
       except Exception as e:
          con.rollback()
          print(e)
          msg = "error in login"
       finally:
         con.close()
-        if correct:
-           return render_template("result.html",msg = msg)
+        if correctSqlinyection:
+           return render_template("resultInyection.html",msg = msg)
+        elif correctDefaultCred:
+           return render_template("resultDefaultCred.html", msg = msg)
         else:
            return render_template('login.html',msg = msg)
    else:
